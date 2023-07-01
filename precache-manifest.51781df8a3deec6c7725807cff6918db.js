a0e083a71f064834ccf0d3318e1350865a37c377
nguyenduytuan625 <nguyenduytuan1600pc@gmail.com>
2023-07-01 12:03:33 +0700

    Updates

---
+++ precache-manifest.51781df8a3deec6c7725807cff6918db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d627fb1aa2c7d7a3590b00df56d5c8be",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "638c27d24dc1c8fb890d",
+    "url": "/react-movie-app/static/css/main.26cdf456.chunk.css"
+  },
+  {
+    "revision": "39eb8975fa301840bb99",
+    "url": "/react-movie-app/static/js/2.b33b9e92.chunk.js"
+  },
+  {
+    "revision": "6ede68811dbbd89c6e15b10089740193",
+    "url": "/react-movie-app/static/js/2.b33b9e92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "638c27d24dc1c8fb890d",
+    "url": "/react-movie-app/static/js/main.b7ebb360.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/react-movie-app/static/js/main.b7ebb360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e94b8089a5e72a69dd2a",
+    "url": "/react-movie-app/static/js/runtime-main.f3742413.js"
+  }
+]);
